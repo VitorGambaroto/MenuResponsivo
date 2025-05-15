d7da7c6f310827fa9c2cf0ac548b4b374fb6262b
VitorGambaroto <vitorgambaroto1@hotmail.com>
2025-05-15 05:08:22 -0300

    projeto quase pronto

---
+++ script.js
@@ -0,0 +1,8 @@
+var clicou = document.getElementById ("clicou")
+function clickdown() {
+    if (clicou.style.display=='block') {
+        clicou.style.display = 'none' 
+    } else {
+        clicou.style.display = 'block'
+    }
+}
